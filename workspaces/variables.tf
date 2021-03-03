d97ba06d7e19fc0559f0cb6a7776d465c46b48aa
Martin Eckhardt <meckhardt@users.noreply.github.com>
2021-03-04 00:39:12 +0100

    Add vcs_repo /2

--- workspaces/variables.tf
+++ workspaces/variables.tf
@@ -3,3 +3,8 @@ variable "organization" {
   type        = string
   default     = "eckhardt-engineering"
 }
+
+variable "vcs_oauth_token" {
+  description = "The vcs oauth token"
+  type        = string
+}
--- workspaces/workspace1.tf
+++ workspaces/workspace1.tf
@@ -1,4 +1,9 @@
 resource "tfe_workspace" "my-app" {
   name = "my-app"
   organization = var.organization
+  working_directory = "app"
+  vcs_repo {
+    identifier = "meckhardt/terraform-cloud"
+    oauth_token_id = var.vcs_oauth_token
+  }
 }
