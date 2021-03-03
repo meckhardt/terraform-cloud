2d9875e136dbbc638cd0554ce660dddf4e62d524
Martin Eckhardt <meckhardt@users.noreply.github.com>
2021-03-03 22:20:16 +0100

    Try speculative plans

--- workspaces/variables.tf
+++ workspaces/variables.tf
@@ -1,25 +1,5 @@
-variable "vcs_repo_oauth_token_id" {
-  description = "Bitbucket oauth Token id"
-  type        = string
-  default     = "ot-4zJgZHSXDpkz6GDt"
-}
-
 variable "organization" {
   description = "tfc organization"
   type        = string
   default     = "eckhardt-engineering"
-}
-
-variable "google_credentials" {
-  description = "development gcp credentials service account without newline"
-  type        = string
-}
-
-variable "billing_account_id" {
-  description = "The ID of the associated billing account (optional)."
-}
-
-variable "site_vpn_shared_secret_development" {
-  description = "Shared secret of the site to site vpn for development"
-  type        = string
 }
--- workspaces/workspace1.tf
+++ workspaces/workspace1.tf
@@ -1,13 +1,4 @@
 resource "tfe_workspace" "my-app" {
   name = "my-app"
-  vcs_repo {
-    identifier     = "DIP/operations-terraform"
-    branch         = "feature/DIA-1458"
-    oauth_token_id = var.vcs_repo_oauth_token_id
-  }
-  trigger_prefixes      = ["/google/global/development"]
-  working_directory     = "/google/global/development"
-  terraform_version     = "0.13.5"
   organization          = var.organization
-  file_triggers_enabled = true
 }
